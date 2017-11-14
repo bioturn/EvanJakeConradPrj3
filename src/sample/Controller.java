14108774751ed1def379b59486b42a716f5ee637
Bioturn <conradthompson@rocketmail.com>
2017-11-13 20:37:52 -0600

    Adding FXML fields to Controller Class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+
+    @FXML
+    private NumberTextField tempInput;
+
+    @FXML
+    private Button outsideTemp;
+
+    @FXML
+    private Label currentTempLabel;
+
+    @FXML
+    void initialize() {}
 }

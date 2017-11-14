b0ead0d27b1ca7f2d1c56f31135e08c24a042cf7
Bioturn <conradthompson@rocketmail.com>
2017-11-13 19:48:00 -0600

    Initial Commit

---
+++ src/sample/NumberTextField.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.scene.control.TextField;
+
+public class NumberTextField extends TextField {
+
+    public NumberTextField () {
+        this.setPromptText("Enter Only Numbers");
+    }
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (text.matches("[0-9]") || text.isEmpty()) {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String replacement) {
+        super.replaceSelection(replacement);
+    }
+}

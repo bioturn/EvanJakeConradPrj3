f840c6e1d9de14bd021a1e4d9ea100c10ee5b63d
Bioturn <conradthompson@rocketmail.com>
2017-12-09 10:05:33 -0600

    Reformatting these out, as I do not think we need them.

--- src/timer/Notifiable.java
+++ src/timer/Notifiable.java
@@ -1,16 +1,5 @@
 package timer;
 
-import events.TimerRanOutEvent;
-import events.TimerTickedEvent;
-
 public interface Notifiable {
-    /**
-     * Process timer ticks
-     */
-    public void handleEvent(TimerTickedEvent event);
 
-    /**
-     * Process timer runs out event
-     */
-    public void handleEvent(TimerRanOutEvent event);
 }

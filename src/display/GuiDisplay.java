ecc4b26d60072429a6496d41bb58a50607272132
Bioturn <conradthompson@rocketmail.com>
2017-12-09 09:01:00 -0600

    Finding a new way to try this timer and clock stuff.

---
+++ src/display/GuiDisplay.java
@@ -0,0 +1,27 @@
+package display;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class GuiDisplay extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/tempControlUnit.fxml"));
+        primaryStage.setTitle("Temperature Control Unit");
+        primaryStage.setScene(new Scene(root, 800,200));
+        primaryStage.show();
+        primaryStage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent window) {
+                System.exit(0);
+            }
+        });
+
+    }
+
+}
--- src/start/Main.java
+++ src/start/Main.java
@@ -5,33 +5,18 @@
 package start;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import states.Clock;
+import display.GuiDisplay;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Clock.instance();
-        Parent root = FXMLLoader.load(getClass().getResource("/view/tempControlUnit.fxml"));
-        primaryStage.setTitle("Temperature Control Unit");
-        primaryStage.setScene(new Scene(root, 800,200));
-        primaryStage.show();
-        primaryStage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent window) {
-                System.exit(0);
-            }
-        });
-    }
-
+public class Main{
 
     public static void main(String[] args) {
-        launch(args);
+        Clock.instance();
+        new Thread() {
+            @Override
+            public void run() {
+                Application.launch(GuiDisplay.class, null);
+            }
+        }.start();
     }
 }
